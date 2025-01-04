1e7c295a608e4983382cd24b21c5ae92a2aed222
usman1100 <usmanahmed1100@gmail.com>
2025-01-05 00:15:52 +0500

    replace module templates with embedded templates

--- lib/liveplace_web/controllers/game_html.ex
+++ lib/liveplace_web/controllers/game_html.ex
@@ -1,15 +1,5 @@
 defmodule LiveplaceWeb.GameHTML do
   use LiveplaceWeb, :html
 
-  def index(assigns) do
-    ~H"""
-    <h1 class="text-6xl">Game Index</h1>
-    """
-  end
-
-  def show(assigns) do
-    ~H"""
-    <h1 class="text-6xl">Game Show</h1>
-    """
-  end
+  embed_templates "game_html/*"
 end
