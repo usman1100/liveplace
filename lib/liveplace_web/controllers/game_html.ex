9c2e701aeb2851c58e3eb6f5c2b692f766be2237
usman1100 <usmanahmed1100@gmail.com>
2025-01-10 14:46:03 +0500

    condtionals

--- lib/liveplace_web/controllers/game_html.ex
+++ lib/liveplace_web/controllers/game_html.ex
@@ -3,8 +3,13 @@ defmodule LiveplaceWeb.GameHTML do
 
   def game_chip(assigns) do
     ~H"""
-    <div :if={@gameid == "123"} class="mt-1 h-fit w-fit px-2 py-1 bg-green-400 rounded-lg">{@gameid}</div>
-    <div :if={@gameid != "123"} class="mt-1 h-fit w-fit px-2 py-1 bg-red-400 rounded-lg">{@gameid}</div>
+    <div class={[
+      "mt-1 h-fit w-fit px-2 py-1 rounded-lg",
+      @gameid == "123" && "bg-red-500 text-white",
+      @gameid != "123" && "bg-gray-200 text-gray-800"
+    ]}>
+      {@gameid}
+    </div>
     """
   end
 
