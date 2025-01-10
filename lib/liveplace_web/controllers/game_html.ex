0d7ee35af2aa1e3959a9ad5041b217420190a0ce
usman1100 <usmanahmed1100@gmail.com>
2025-01-10 14:35:43 +0500

    custom component

--- lib/liveplace_web/controllers/game_html.ex
+++ lib/liveplace_web/controllers/game_html.ex
@@ -1,5 +1,12 @@
 defmodule LiveplaceWeb.GameHTML do
   use LiveplaceWeb, :html
 
+  def game_chip(assigns) do
+    ~H"""
+    <div :if={@gameid == "123"} class="mt-1 h-fit w-fit px-2 py-1 bg-green-400 rounded-lg">{@gameid}</div>
+    <div :if={@gameid != "123"} class="mt-1 h-fit w-fit px-2 py-1 bg-red-400 rounded-lg">{@gameid}</div>
+    """
+  end
+
   embed_templates "game_html/*"
 end
