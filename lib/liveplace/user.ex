49c77f4ec25a107964d7fa6d6a8d49011b94ec52
usman1100 <usmanahmed1100@gmail.com>
2025-01-11 15:06:49 +0500

    users table

---
+++ lib/liveplace/user.ex
@@ -0,0 +1,20 @@
+defmodule Liveplace.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :age, :integer
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :age])
+    |> validate_required([:name, :email])
+    |> validate_length(:name, min: 2)
+  end
+end
